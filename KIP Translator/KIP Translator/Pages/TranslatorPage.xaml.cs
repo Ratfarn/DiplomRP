fe5970b01bf967d85d29981149218ac03dce6f0b
Ratfarn <farart98@gmail.com>
2023-01-20 15:20:46 +0300

    V1

---
+++ KIP Translator/KIP Translator/Pages/TranslatorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KIP_Translator.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TranslatorPage.xaml
+    /// </summary>
+    public partial class TranslatorPage : Page
+    {
+        public TranslatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TextWrite_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextRead.Text = TextWrite.Text;
+        }
+    }
+}
