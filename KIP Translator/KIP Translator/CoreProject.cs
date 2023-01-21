09fa00560d43e2720d1fe3b3ca127ecad2ef5b63
Ratfarn <farart98@gmail.com>
2023-01-21 15:37:40 +0300

    V2

---
+++ KIP Translator/KIP Translator/CoreProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIP_Translator
+{
+    internal class CoreProject
+    {
+        /* 
+         * 
+         * место для подключения к базе данных
+         * 
+         */
+    }
+}
